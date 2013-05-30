3edd5086538e4665a6058b4bbf169e828dda2728
xnzhu <xiaonaz@gmail.com>
2013-05-30 17:57:35 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SqaSchool::Application.config.secret_token = 'cfe50de734ea96a6192f8c842794c74933e0a0cf0f901c5240ab39614ed0ac15722c30b1a9b97750b75fdb2e7795c922f288eba5d75846b35893ac232a5020d0'
